2dae042ce4f6ae9abe7529e7d18b801273d591cb
chris mendez mba <mendecj812@gmail.com>
2025-10-15 21:22:22 -0500

    feat: add CRUD functions

---
+++ client/services/CarsAPI.jsx
@@ -0,0 +1,9 @@
+export async function getAllCars() {}
+
+export async function getCar() {}
+
+export async function createCar() {}
+
+export async function deleteCar() {}
+
+export async function updateCar() {}
