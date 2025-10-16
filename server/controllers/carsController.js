ec90120eaf14f54f5550ef2af7a57dfa9eb933fa
Chris Mendez <mendecj812@gmail.com>
2025-10-16 13:40:06 -0500

    feat: implement backend controllers

--- server/controllers/carsController.js
+++ server/controllers/carsController.js
@@ -1,13 +1,63 @@
-import { pool } from "../config/database";
+import { pool } from '../config/database.js';
 
-function getCars() {}
+async function getCars(req, res) {
+  try {
+    const results = await pool.query(`SELECT * FROM cars`);
+    res.status(201).json(results.rows);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+}
 
-function getCar() {}
+async function getCar(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const results = await pool.query(`SELECT * FROM cars WHERE id = $1`, [id]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+}
 
-function createCar() {}
+async function createCar(req, res) {
+  try {
+    const { name, exterior_color, roof, wheels, interior, price } = req.body;
+    const results = await pool.query(
+      `
+  INSERT INTO cars (name, exterior_color, roof, wheels, interior, price)
+  VALUES($1, $2, $3, $4, $5, $6)
+  RETURNING *`,
+      [name, exterior_color, roof, wheels, interior, price]
+    );
+    res.status(201).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+}
 
-function updateCar() {}
+async function updateCar(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const { name, exterior_color, roof, wheels, interior, price } = req.body;
+    const results = await pool.query(
+      `
+          UPDATE cars SET name = $1, exterior_color = $2, roof = $3, wheels = $4, interior = $5, price = $6 WHERE id = $7`,
+      [name, exterior_color, roof, wheels, interior, price, id]
+    );
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+}
 
-function deleteCar() {}
+async function deleteCar(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const results = await pool.query('DELETE FROM cars WHERE id = $1', [id]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+}
 
 export { getCars, getCar, createCar, updateCar, deleteCar };
