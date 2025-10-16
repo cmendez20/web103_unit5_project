4a1a2203a6f292154a02e25f1e11069146155b75
chris mendez mba <mendecj812@gmail.com>
2025-10-15 21:14:21 -0500

    feat: create controller signatures

---
+++ server/controllers/carsController.js
@@ -0,0 +1,13 @@
+import { pool } from "../config/database";
+
+function getCars() {}
+
+function getCar() {}
+
+function createCar() {}
+
+function updateCar() {}
+
+function deleteCar() {}
+
+export { getCars, getCar, createCar, updateCar, deleteCar };
